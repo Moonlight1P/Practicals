54f8da6ec2ed4a44080b94022989a5bf10a77570
Moonlight1P <110157954+Moonlight1P@users.noreply.github.com>
2022-07-28 13:03:04 +0530

    Create expt2.java

---
+++ oop lab/expt2.java
@@ -0,0 +1,19 @@
+public class expt2
+{
+  public static void main(String[] args)
+  {
+     System.out.println("Prime numbers from 1 to 1000 are:");
+     int i,j;
+	 for(i=1;i<=1000;i++)
+	 {
+	   int c=0;
+	   for(j=1;j<=i;j++)
+	   {
+	     if(i%j==0)
+		 ++c;
+	   }
+        if(c==2)
+           System.out.println(i);
+	 }
+  }
+}  
